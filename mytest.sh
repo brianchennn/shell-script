7aa8655942d9ebf3ffa8b3b220d788c143a928b0
Charlie Root <root@test.brian>
2020-04-30 07:38:24 +0800

    modify mytest.sh

--- mytest.sh
+++ mytest.sh
@@ -2,14 +2,15 @@ dig @10.113.50.2 +noall +answer ns1.0716211.nasa.
 dig @10.113.50.2 +noall +answer ns2.0716211.nasa.
 dig @10.113.50.2 +noall +answer router.0716211.nasa.
 dig @10.113.50.2 +noall +answer agent.0716211.nasa.
+
 dig @10.113.50.2 +noall +answer -x 10.113.50.1
-dig @10.113.50.2 +noall +answer 10.113.50.2
-dig @10.113.50.2 +noall +answer 10.113.50.254
-dig @10.113.50.2 +noall +answer 10.113.50.129
+dig @10.113.50.2 +noall +answer -x 10.113.50.2
+dig @10.113.50.2 +noall +answer -x 10.113.50.254
+dig @10.113.50.2 +noall +answer -x 10.113.50.129
 dig @10.113.50.2 +noall +answer soa 0716211.nasa.
 dig @10.113.50.2 +noall +answer soa nasa.
 dig @10.113.50.2 +noall +answer -x 10.113.50.87
 dig @10.113.50.2 +noall +answer version.bind txt chaos
 dig @10.113.50.2 +noall +answer web.0716211.nasa. CNAME
-dig @10.113.50.2 +noall +answer router.0716211.nasa. CNAME
+dig @10.113.50.2 +noall +answer nasa CNAME
 
